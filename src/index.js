f537f67f2de05a00dbd31df44fde2ae9dd8bcbf7
provedenceva <anyuta-provedenc@mail.ru>
2018-09-18 23:38:08 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
+  var count = 0;
+  var k = 1;
+  do {
+    count += Math.floor(number / Math.pow(5, k));
+    k++;
+  }
+  while (Math.pow(5, k) < number) 
+  return count;
+  }
   // your implementation
-}
+
